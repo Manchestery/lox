bd218e980118352f01159b0064d038fc5c559d8b
yx <9124354@qq.com>
2025-10-25 22:08:03 +0800

    finish the clox scanner

--- clox/vm.h
+++ clox/vm.h
@@ -18,7 +18,7 @@ typedef enum {
 } InterpretResult;
 void initVM();
 void freeVM();
-InterpretResult interpret(Chunk* chunk);
+InterpretResult interpret(const char* source);
 void push(Value value);
 Value pop();
 #endif
